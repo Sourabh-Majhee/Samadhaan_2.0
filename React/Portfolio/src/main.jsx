d600d3d9f777272a603e182e4961696d72eb49f3
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-08-24 20:03:59 +0530

    Created basic files for working on the portfolio

--- React/Portfolio/src/main.jsx
+++ React/Portfolio/src/main.jsx
@@ -1,7 +1,18 @@
+/*
+    @copyright - 2025
+    @licence - Sourabh Majhee
+*/
+
+// Node Modules
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// Components
 import App from './App.jsx'
+
+// Css link 
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
