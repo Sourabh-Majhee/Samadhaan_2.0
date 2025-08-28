08abad0b8c9bd13bb4367dd53e566700fe1b1563
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-08-28 22:37:31 +0530

    Solved an error in task of day 5

--- DailyTasks/Day5/server.js
+++ DailyTasks/Day5/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Dummy student data
-const student = [
+const students = [
   { id: 1, name: "Rahul Sharma", age: 20, course: "Computer Science" },
   { id: 2, name: "Priya Verma", age: 21, course: "Information Technology" },
   { id: 3, name: "Amit Singh", age: 19, course: "Electronics" },
